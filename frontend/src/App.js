a7b066e18ad558e6e31d86ff3d572f6939f5d94d
Argyris3 <bigol3@hotmail.com>
2023-03-06 17:32:02 +0200

    master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,39 @@
+import data from './data';
+import {BrowseRouter,Route,Routes} from "react-router-dom"
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowseRouter>
+      <div>
+        <header>
+          <a href="/">amazona</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen/>}/>
+          </Routes>
+          <h1>Featured Products</h1>
+          <div className="products">
+            {data.products.map((product) => (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p>
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button>Add to Cart</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </main>
+      </div>
+    </BrowseRouter>
   );
 }
 
